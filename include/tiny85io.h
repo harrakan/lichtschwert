9f25963cc9f5881b2b6552052c7fac7a1f932f1b
philipp.hildebrandt@posteo.de <philipp.hildebrandt@posteo.de>
2015-12-24 15:09:21 +0100

    moved defines to header

--- include/tiny85io.h
+++ include/tiny85io.h
@@ -7,6 +7,10 @@
 #define HIGH 0x1
 #define LOW  0x0
 
+#define INPUT 0x0
+#define OUTPUT 0x1
+#define INPUT_PULLUP 0x2
+
 void initADC(void);
 uint16_t analogRead(int8_t channel);
 void digitalWrite(uint8_t pin, uint8_t val);
